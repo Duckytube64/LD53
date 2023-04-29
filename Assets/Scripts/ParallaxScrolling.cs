19e8759dbf64bdbecb8e20f67b024915c01a461b
RSBZeph <RSBZeph@gmail.com>
2023-04-29 22:03:21 +0200

    parallax scrolling and own art added

---
+++ Assets/Scripts/ParallaxScrolling.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxScrolling : MonoBehaviour
+{
+    Transform cam;
+    Vector3 camStartPosition;
+    float distance;
+    Material mat;
+    float speed = 0.02f;
+
+    float farthestBack;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main.transform;
+        camStartPosition = cam.position;
+        mat = GetComponent<Renderer>().material;
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = new Vector3(cam.position.x, cam.position.y, 0);
+        distance = cam.position.x - camStartPosition.x;
+        mat.SetTextureOffset("_MainTex" , new Vector2(distance,0) * speed);
+    }
+}
