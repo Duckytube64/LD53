0ca8989d0fc5006ff509968e7f0332c7127656b2
Jeroen Hijzelendoorn <j.m.hijzelendoorn@students.uu.nl>
2023-04-29 21:56:25 +0200

    Working on throw improvement

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -8,16 +8,22 @@ public class Main : MonoBehaviour
     [SerializeField]
     FollowCamera followCamera;
     [SerializeField]
-    PlayerController knight, frog;
+    PlayerController knightCon, frogCon;
     Transform knightT, frogT;
-    float throwForce = 100;
+    Rigidbody2D knightR, frogR;
+    CircleCollider2D frogCol;
+
+    public float throwForce = 10;
     bool frogGrabbed = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        knightT = knight.gameObject.transform;
-        frogT = frog.gameObject.transform;
+        knightT = knightCon.gameObject.transform;
+        frogT = frogCon.gameObject.transform;
+        frogR = frogCon.gameObject.GetComponent<Rigidbody2D>();
+        frogCol = frogCon.gameObject.GetComponent<CircleCollider2D>();
+        frogCol.enabled = false;
     }
 
     // Update is called once per frame
@@ -30,48 +36,62 @@ public class Main : MonoBehaviour
         {
             if (frogGrabbed)
                 DropFrog();
-            else if (knight.isControlled && (knightT.position - frogT.position).magnitude < 0.75f)
+            else if (knightCon.isControlled && (knightT.position - frogT.position).magnitude < 0.75f)
                 GrabFrog();
         }
 
         // Throw frog
         if (Input.GetMouseButtonDown(0) && frogGrabbed)
-        {
-            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector3 diffN = (worldPosition - knightT.position).normalized * throwForce;
-            frog._currentHorizontalSpeed += diffN.x;
-            frog._currentVerticalSpeed += diffN.y;
-            DropFrog();
-        }
+            ThrowFrog();
     }
 
     void ChangeToKnight()
     {
-        frog.isControlled = false;
-        knight.isControlled = true;
+        frogCon.isControlled = false;
+        knightCon.isControlled = true;
         followCamera.SetCameraWeightTarget(0);
     }
 
     void ChangeToFrog()
     {
         if (frogGrabbed) return;
-        knight.isControlled = false;
-        frog.isControlled = true;
+        knightCon.isControlled = false;
+        frogCon.isControlled = true;
         followCamera.SetCameraWeightTarget(1);
+
+        frogR.gravityScale = 0;
+        frogR.drag = 0;
+        //frogR.angularVelocity = Vector3.zero;
+        frogR.velocity = Vector3.zero;
+        frogCon.enabled = true;
+        frogCol.enabled = false;
     }
 
     void GrabFrog()
     {
         frogT.parent = knightT;
         frogT.localPosition = Vector3.zero;
-        frog.isKinematic = true;
+        frogCon.isKinematic = true;
         frogGrabbed = true;
     }
 
     void DropFrog()
     {
         frogT.parent = GameObject.Find("Player characters").transform;
-        frog.isKinematic = false;
+        frogCon.isKinematic = false;
         frogGrabbed = false;
+    }
+
+    void ThrowFrog()
+    {
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        worldPosition.z = 0;
+        Vector3 diffN = (worldPosition - knightT.position).normalized * throwForce;
+        frogR.gravityScale = 2;
+        frogR.drag = 1.5f;
+        frogCon.enabled = false;
+        frogCol.enabled = true;
+        DropFrog();
+        frogR.AddForce(diffN);
     }
 }
